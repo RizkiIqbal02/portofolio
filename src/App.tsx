620a108bbb134a7ebe5ff40d355c4e980ee1580b
RizkiIqbal02 <roxashiqbal12@gmail.com>
2025-10-19 13:45:34 +0700

    define grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,13 @@ function App() {
   const words = `Oxygen gets you high. In a catastrophic emergency, we're taking giant, panicked breaths. Suddenly you become euphoric, docile. You accept your fate. It's all right here. Emergency water landing, six hundred miles an hour. Blank faces, calm as Hindu cows
 `;
   return (
-    <>
-      <h5 className="font-bold">hello</h5>
-      <TextGenerateEffect words={words} />
-    </>
+    <main>
+      <section className="grid grid-cols-12">
+        <div className="col-span-8">
+          <TextGenerateEffect words={words} />
+        </div>
+      </section>
+    </main>
   );
 }
 
